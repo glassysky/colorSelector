62f41a50fbebfe2de8859586f663725cfcdfc4c0
glassysky <873661157@qq.com>
2016-03-04 14:55:41 +0800

    webpack init

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,24 @@
-module.exports = require("./make-webpack-config")({
+var webpack = require('webpack');
+var commonsPlugin = new webpack.optimize.CommonsChunkPlugin('common.js');
 
-});
+module.exports = {
+	plugins: [commonsPlugin],
+	entry: {
+		index: './app/src/js/page/index.js'
+	},
+	output: {
+		path: './app/dist/js/page',
+		filename: '[name].bundle.js'// name => entry's key
+	},
+	module: {
+		loaders: [
+			{ test: /\.css$/, loader: 'style-loader!css-loader' },
+			{ test: /\.js$/, loader: 'jsx-loader?harmony' },
+			{ test: /\.less$/, loader: 'style-loader!css-loader!less-loader?sourceMap' },
+			{ test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192' },
+		]
+	},
+	resolve: {
+		extensions: ['', '.js', '.json', '.less']
+	}
+}
